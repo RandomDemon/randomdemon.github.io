5020e0508590307f9d9bf7b0ce04e94a72f897e3
RandomDemon <104325602+RandomDemon@users.noreply.github.com>
2022-12-29 22:59:20 -0500

    Update joke_bot.py

--- joke_bot.py
+++ joke_bot.py
@@ -9,7 +9,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if message.content == '/programmer-joke':
-        jokes = [
+        programmer-jokes = [
             "Why do programmers always mix up Halloween and Christmas? Because Oct 31 equals Dec 25.",
             "Why was the JavaScript developer sad? Because he didn't know how to 'null' his feelings.",
             "Why do programmers hate nature? It has too many bugs.",
@@ -21,7 +21,7 @@ async def on_message(message):
             "Why was the computer wet? It left its Windows open during a rainstorm.",
             "Why did the computer go to therapy? It was suffering from CPU stress."
         ]
-        random_joke = jokes[random.randint(0, len(jokes) - 1)]
+        random_joke = programmer-jokes[random.randint(0, len(jokes) - 1)]
         await message.channel.send(random_joke)
 
 client.run('MTA1ODIzMjAyNzkwOTk4ODM3Mg.GKBs70.Y211I0niusBd3hAq8S7wie_gLGiVt-cNlrwzpM')
